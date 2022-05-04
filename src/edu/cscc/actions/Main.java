33e88235984ba262bec0f4aeeee30432e607a9e4
Andrew Friend <mandrewfriend@gmail.com>
2022-05-04 16:00:55 -0400

    Did another update for the PR

--- src/edu/cscc/actions/Main.java
+++ src/edu/cscc/actions/Main.java
@@ -2,7 +2,6 @@ package edu.cscc.actions;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello Universe!");
-
+		System.out.println("Hello Multiverse!");
 	}
 }
